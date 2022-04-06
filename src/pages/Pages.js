f3d6fb63bce2f1ed89f513a1283cc49bf90af65d
gordongecco <delldani001@gmail.com>
2022-04-06 14:51:48 +0200

    ref

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -10,19 +10,13 @@ import { UserContext } from "../context";
 import { NotFound } from "./NotFound";
 
 export const Pages = () => {
-  const contextObject = React.useContext(UserContext);
-  const { job } = contextObject;
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/registration" element={<RegistrationPage />} />
-      {job !== "parent" && (
-        <Route path="/permission" element={<PermissionPage />} />
-      )}
-
-      {job === "director" && <Route path="/list" element={<ListPage />} />}
-
-      {job !== "director" && <Route path="/wallet" element={<WalletPage />} />}
+      <Route path="/permission" element={<PermissionPage />} />
+      <Route path="/list" element={<ListPage />} />
+      <Route path="/wallet" element={<WalletPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
